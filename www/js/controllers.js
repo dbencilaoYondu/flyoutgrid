3ee70d4e0a4ab301513049d802fc98748f15714e
Don Bencilao <Don Bencilao@egg.ph>
2016-03-03 15:59:14 +0800

    build 1.6

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-var app = angular.module('starter.controllers', [])
+var app = angular.module('starter.controllers', ['youtube-embed'])
 
 
 
@@ -202,6 +202,12 @@ app.controller('SettingsCtrl',function($scope,$ionicModal,Pages, $ionicHistory){
     $scope.data = Pages;
     Pages.getSpecs();
 
+    if($scope.data.data.data.headerText){
+      $scope.headerText = $scope.data.data.data.headerText;
+    }else{
+      $scope.headerText = $scope.data.data.data.applicationName;
+    }
+
      $ionicModal.fromTemplateUrl('settings.html', {
       id: '1', // We need to use and ID to identify the modal that is firing the event!
       scope: $scope,
@@ -267,7 +273,7 @@ app.controller('SettingsCtrl',function($scope,$ionicModal,Pages, $ionicHistory){
 });
 
 app.controller('BlankCtrl',function($scope,Pages,$timeout){
-
+  $scope.data = Pages;
   $timeout(function() {
     
      if($scope.data.data.data.previewObj.status == true){
@@ -277,8 +283,6 @@ app.controller('BlankCtrl',function($scope,Pages,$timeout){
       }else{
         console.log("previewStatus false");
       }
-
-
   }, 10);
   
 
@@ -286,6 +290,7 @@ app.controller('BlankCtrl',function($scope,Pages,$timeout){
 
   console.log($scope);
 });
+
 app.controller('AboutCtrl', function($scope,$ionicModal,Pages,$state) {
 
       $scope.data = Pages;
@@ -500,6 +505,65 @@ app.controller('GalleryCtrl', function($scope,$stateParams,$state, Pages) {
     console.log($scope.currentParentOfSubInfo);
 
 });
+
+
+
+
+app.controller('VideoCtrl', function($scope,$state, $http, Pages){
+    $scope.data = Pages;
+    $scope.currentData = $state.current.data;
+
+    //transfer data to sub rss pages
+    if($scope.data.scrum2[$scope.currentData]){
+      //set data to parent rss pages
+      $scope.currentVideoData = $scope.data.scrum2[$scope.currentData];
+    }else{
+       $scope.currentVideoData = $scope.currentParentOfSubInfo;
+    }
+
+    console.log('video ctrl');
+    console.log($scope);
+
+     $scope.youtubeParams = {
+         key: $scope.currentVideoData.youtube.key,
+         type: 'video',
+         maxResults: $scope.currentVideoData.youtube.resultLimit,
+         part: 'id,snippet',
+         order: 'date',
+         //forUsername: 'aybutchikik',
+         channelId: $scope.currentVideoData.youtube.channelId
+     }
+     
+     $http.get('https://www.googleapis.com/youtube/v3/search', {
+
+        params: $scope.youtubeParams
+
+      })
+
+      .success(function(response){
+
+        $scope.videos  = response.items;
+
+        angular.forEach(response.items, function(child){
+             console.log (child);
+        });
+      });
+       
+      $scope.playerVars = {
+       rel: 0,
+       showinfo: 0,
+       modestbranding: 0,
+      }
+});
+
+
+
+
+
+  
+
+
+
 app.controller('MapCtrl', function($scope ,$state, Pages,$cordovaGeolocation) {
   var options = {timeout: 10000, enableHighAccuracy: true};
  
